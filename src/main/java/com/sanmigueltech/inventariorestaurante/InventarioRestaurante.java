41fadc4b79f84767a7215e0dd977ae675d7efbd5
Alejandro Sanmiguel <alejandrosanmiguel0222@gmail.com>
2023-11-01 19:24:57 -0500

    Creación del proyecto

---
+++ src/main/java/com/sanmigueltech/inventariorestaurante/InventarioRestaurante.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.sanmigueltech.inventariorestaurante;
+
+/**
+ *
+ * @author aleja
+ */
+public class InventarioRestaurante {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
